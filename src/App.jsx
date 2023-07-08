80c1ab719799c6d6d99587d8f86faea034ffeb6a
Aadish <500094812@stu.upes.ac.in>
2023-07-08 14:24:15 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Aadish Bansal",
+  title: "Founder(Aadi Foundation) , Web Designer & Content Creator",
+  email: "aadish.bansal@studentambassador.com",
+  gitHub: "AadishBansal12",
+  instagram: "aadishbansal1",
+  linkedIn: "aadishbansal1",
+  medium: "Aadishbansal",
+  twitter: "",
+  youTube: "Aadish Bansal",
 };
 
 const primaryColor = "#4E567E";
